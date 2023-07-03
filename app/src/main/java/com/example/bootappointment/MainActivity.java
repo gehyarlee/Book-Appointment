c706291965a6819988873f291f81083a78dc0923
Lee Gehyarlee <99612857+gehyarlee@users.noreply.github.com>
2023-07-04 01:48:35 +0200

    files upload
    This is simple project for booking appointment using android studio.

---
+++ app/src/main/java/com/example/bootappointment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bootappointment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
